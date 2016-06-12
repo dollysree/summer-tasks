f63e274618f46b427a1062a89a341b1e77962d66
dollysree <sneha.kanaprthi379@gmail.com>
2016-06-11 19:38:49 -0700

    model file is added

---
+++ task-1/phase-3/models/Website_model.php
@@ -0,0 +1,44 @@
+<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>-->
+<?php
+class Website_model extends CI_Model {
+
+        public function __construct()
+        {
+                $this->load->database();
+        }
+		public function get_news($slug = FALSE)
+{
+        if ($slug === FALSE)
+        {
+                $query = $this->db->get('news');
+                return $query->result_array();
+        }
+
+        $query = $this->db->get_where('news', array('slug' => $slug));
+        return $query->row_array();
+}
+public function set_news()
+{
+    $this->load->helper('url');
+
+    $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+    $data = array(
+        'title' => $this->input->post('title'),
+        'slug' => $slug,
+        'text' => $this->input->post('text')
+    );
+
+    return $this->db->insert('news', $data);
+}
+}
+?>
+</body>
+</html>
