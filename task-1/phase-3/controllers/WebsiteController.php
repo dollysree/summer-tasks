7d750fcf9547911abd9dc6e730cce1f02de4cf83
dollysree <sneha.kanaprthi379@gmail.com>
2016-06-11 19:37:48 -0700

    Controller is added

---
+++ task-1/phase-3/controllers/WebsiteController.php
@@ -0,0 +1,89 @@
+<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>-->
+<?php
+class WebsiteController extends CI_Controller {
+
+        public function __construct()
+        {
+                parent::__construct();
+                $this->load->model('Website_model');
+                $this->load->helper('url_helper');
+        }
+
+       public function index()
+{
+        $data['news'] = $this->Website_model->get_news();
+        $data['title'] = 'News Website';
+
+        $this->load->view('website/navbar.html', $data);
+      
+}
+
+        public function view($slug = NULL)
+        {
+                $data['news_item'] = $this->Website_model->get_news($slug);
+				
+				if (empty($data['news_item']))
+        {
+                show_404();
+        }
+
+        $data['title'] = $data['news_item']['title'];
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('website/view', $data);
+        $this->load->view('templates/footer');
+        }
+		public function addarticle()
+{
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+
+    $data['title'] = 'Create a news item';
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('textarea', 'Text', 'required');
+
+    if ($this->form_validation->run() === FALSE)
+    {
+        $this->load->view('website/addarticle');
+  
+
+    }
+    else
+    {
+        $this->Website_model->set_news();
+        $this->load->view('website/success');
+    }
+}
+	public function login()
+{
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+
+
+
+    $this->form_validation->set_rules('username', 'username', 'required');
+    $this->form_validation->set_rules('password', 'password', 'required');
+
+    if ($this->form_validation->run() === FALSE)
+    {
+        $this->load->view('website/login');
+  
+
+    }
+    else
+    {
+        $this->load->view('website/admin.html');
+    }
+}
+}
+?>
+</body>
+</html>
