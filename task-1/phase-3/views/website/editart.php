4f756788fa387346831a80df12cb5506751ce719
dollysree <sneha.kanaprthi379@gmail.com>
2016-06-12 20:44:09 -0700

    few files are added to view folder

---
+++ task-1/phase-3/views/website/editart.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Edit article</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+ 
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<link rel="stylesheet"; type="text/css" href="/css/addart.css"; />
+</head>
+<body>
+ <div class="container-fluid">
+ <div class="header">
+<h1> THE NEWS WEBSITE</h1>
+ </div>
+ </div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+   <!--logo
+   <div class="navbar-header">
+    <a href="#" class="navbar-brand">the news website</a>
+   </div>-->
+   <div>
+     <ul class="nav navbar-nav">
+       <li><a href="#">Home</a></li>
+       <li><a href="#">Articles</a></li>
+     </ul>
+     <ul class="nav navbar-nav navbar-right">
+     <li><a href="navbar.html">Logout</a></li>
+     </ul>
+   </div>
+  </div>
+</nav>
+<div class="container-fluid">
+ <div style="font-size:24px; color:#C00; font:bold; border:1px solid #000; padding:10px;">
+ <div class="row">
+ <div class="col-md-6">Article[Edit]</div>
+ <!-- <div class="col-md-6 pull-right">
+  <div class="col-md-2">
+  <img  src="Save-icon.png" class="img-responsive" /></div>
+  <div class="col-md-2">
+  <img src="apply.png" class="img-responsive" /></div>
+  <div class="col-md-2">
+  <img src="cancel.jpg" class="img-responsive" /></div>
+  </div></div>-->
+   </div></div>  
+</div>
+ <br />
+ <div class="container-fluid">
+ <div style="border:1px solid #000; padding:1px;">
+  <div class="row">
+    <div class="col-md-7" style="background-color:#e7e7e7; border:1px solid #555; margin-left:50px; margin-right:50px; margin-top:25px; padding:10px">
+       <div class="col-md-6 ">
+          <table class="table">
+          <?php echo validation_errors(); ?>
+
+        <?php echo form_open('website/editart'); ?>
+            <tr>
+              <td>Title</td>
+               <td><input type="text" name="title" /></td>
+            </tr> 
+            <tr> 
+               <td>Section</td>
+               <td><select>
+                   <option>Uncategorised</option>
+                   <option>categorised</option>
+                   </select>
+                </td> 
+           </tr>
+         </table>  
+       </div>
+        <div class="col-md-6">
+          <table class="table">
+            <tr>
+              <td>Published</td>
+               <td><input type="radio" value="g2" name="g1" />&nbsp;Yes </td>
+               <td><input type="radio" value="g2" name="g1" />&nbsp;No</td>
+            </tr> 
+            <tr> 
+               <td>Front Page</td>
+               <td><input type="radio" value="g2" name="g2" />&nbsp;Yes </td>
+               <td><input type="radio" value="g2" name="g2" />&nbsp;No</td>
+            </tr> 
+         </table>  
+       </div>
+    </div>
+    <div class="col-md-3" style="border:1px solid #000; margin-top:10px; margin-left:50px; margin-right:50px" >
+     <table class="table">
+            <tr>
+              <td>Article ID</td>
+               <td><input type="text" name="id" /></td>
+            </tr> 
+             <tr>
+              <td>Status</td>
+               <td><input type="text" name="status" /></td>
+            </tr> 
+            <tr>
+              <td>Revised</td>
+               <td><input type="number" name="revised"  /></td>
+            </tr> 
+        </table>    
+     </div>
+  </div> 
+  <div class="row">
+     <div class="col-md-7" style=" margin-left:25px; margin-right:25px">
+     Content:
+  <table class="table">
+    <tr>
+      <td>
+         <textarea name="textarea" style="width:100%; height:200px;" name="content" ></textarea></td>
+     </tr>
+     <tr>    
+         <td>
+         <input type="submit" value="submit" name="button" /></td> 
+      </tr>
+      </table> 
+      </div></div> 
+ </div>
+ 
+ </div>
+
+</body>
+</html>
